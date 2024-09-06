9c007fd89919f88ea4f71cff7f00e330a0385242
jayesh <jayeshsarvaiya9913@gamil.com>
2024-09-06 18:31:11 +0530

    add content

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -30,6 +30,7 @@ const PostList = () => {
         {posts.map((post) => (
           <li key={post._id}>
             <Link to={`/posts/${post._id}`}>{post.title}</Link>
+            <h3>{post.content}</h3>
           </li>
         ))}
       </ul>
